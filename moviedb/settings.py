a3df1b1dfa3a83dc4bb252c9fca75e7f4a75600c
Ant0nyJM <antonyjm1997@gmail.com>
2019-10-29 12:42:27 +0530

    oct-29-1242

--- moviedb/settings.py
+++ moviedb/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'movieapp.apps.MovieappConfig',
     'widget_tweaks',
-    'debug_toolbar',
+    #'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
     #'movieapp.middleware.ProfilerMiddleware',
 ]
 
